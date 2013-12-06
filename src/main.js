122d59163bb9b54eec78f94bd03e2ea0a5daacb2
Iaroslav Sergieiev <noomorph@gmail.com>
2013-12-06 19:35:14 +0200

    Hacking with Wix editor

--- src/main.js
+++ src/main.js
@@ -1,13 +1,13 @@
-define(["knockout", "js/tasty_widget"], function(ko, TastyWidget) {
+define(["knockout", "js/tasty_widget"], function (ko, TastyWidget) {
   window.widget = new TastyWidget({
     style: {
-      width: 320,
-      height: 480,
+      width: window.innerWidth,
+      height: window.innerHeight,
       border: {
-        top:    { width: 1, style: 'solid', color: 'black' },
-        right:  { width: 2, style: 'dashed', color: 'black' },
-        bottom: { width: 3, style: 'dotted', color: 'black' },
-        left:   { width: 4, style: 'solid', color: 'gray' }
+        top:    { width: 1, style: "solid", color: "black" },
+        right:  { width: 2, style: "dashed", color: "black" },
+        bottom: { width: 3, style: "dotted", color: "black" },
+        left:   { width: 4, style: "solid", color: "gray" }
       }
     }
   });
